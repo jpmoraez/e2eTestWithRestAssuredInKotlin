3f5ff95c7abeb3cca9165710958421842551ffcd
jpmoraez <jonamep@gmail.com>
2020-09-15 09:45:33 -0300

    first api test with rest assured

--- src/test/kotlin/e2eTestWithRestAssuredInKotlin/AppTest.kt
+++ src/test/kotlin/e2eTestWithRestAssuredInKotlin/AppTest.kt
@@ -4,10 +4,15 @@
 package e2eTestWithRestAssuredInKotlin
 
 import org.hamcrest.CoreMatchers.*
-import org.junit.Assert.*
-import kotlin.test.Test
+import org.junit.jupiter.api.Test
 
 class AppTest {
-    @Test fun testAppHasAGreeting() {
+
+    @Test fun testListUsers() {
+        io.restassured.RestAssured.`when`()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("page", `is`(2))
     }
 }
